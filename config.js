abef819f414e39a16fa4c383a42c33e36a53ce55
PineRay <matsudaterutaka@gmail.com>
2020-04-10 13:15:47 +0900

    Add rating utility functions.

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ const JOGOO_CONFIG = {
         database: process.env.JOGOO_DB_NAME || 'jogoodb',
         password: process.env.JOGOO_DB_AUTH || 'postgres',
         port: process.env.JOGOO_DB_AUTH || 5432
-    }
+    },
+    JOGOO_RATING_PURCHASED: 1.0,
+    JOGOO_RATING_CLICK_INITIAL: 0.7,
+    JOGOO_RATING_CLICK_INCREASE: 0.01,
+    JOGOO_RATING_NOT_INTERESTED: -1.0
 };
 
 module.exports = JOGOO_CONFIG;
