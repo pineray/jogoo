17d1b26d62cd17b7f1a80db51be2e3f2558669fc
PineRay <matsudaterutaka@gmail.com>
2020-04-14 10:22:56 +0900

    Add an update class to exports.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Jogoo = require('./lib/jogoo');
 const JogooItem = require('./lib/item');
 const JogooClient = require('./lib/client');
+const JogooUpdate = require('./lib/update');
 
 module.exports = {
     Jogoo: Jogoo,
     JogooItem: JogooItem,
     JogooClient: JogooClient,
+    JogooUpdate: JogooUpdate
 };
