ef5fe1507f96f27bf6d7a15b4857a6d5d65ecace
PineRay <matsudaterutaka@gmail.com>
2020-04-16 18:36:01 +0900

    Remove begin and commit command from install query.

--- src/lib/install.ts
+++ src/lib/install.ts
@@ -13,8 +13,7 @@ export class JogooInstall {
         try {
             await this.client.beginTransaction();
 
-            const installSQL = `BEGIN;
-CREATE TABLE jogoo_ratings(member_id INTEGER,product_id INTEGER,category INTEGER,rating FLOAT,ts TIMESTAMP);
+            const installSQL = `CREATE TABLE jogoo_ratings(member_id INTEGER,product_id INTEGER,category INTEGER,rating FLOAT,ts TIMESTAMP);
 CREATE INDEX jogoo_ratings_member_id_index ON jogoo_ratings(member_id);
 CREATE INDEX jogoo_ratings_product_id_index ON jogoo_ratings(product_id);
 CREATE INDEX jogoo_ratings_category_index ON jogoo_ratings(category);
@@ -26,8 +25,7 @@ CREATE INDEX jogoo_ads_products_category_index ON jogoo_ads_products(category);
 CREATE TABLE jogoo_links(item_id1 INTEGER,item_id2 INTEGER,category INTEGER,cnt INT,diff_slope FLOAT);
 CREATE INDEX jogoo_links_item_id1_index ON jogoo_links(item_id1);
 CREATE INDEX jogoo_links_item_id2_index ON jogoo_links(item_id2);
-CREATE INDEX jogoo_links_category ON jogoo_links(category);
-COMMIT;`;
+CREATE INDEX jogoo_links_category ON jogoo_links(category);`;
 
             await this.client.query(installSQL);
             await this.client.commit();
